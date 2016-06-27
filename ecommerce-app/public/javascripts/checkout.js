ccdab169031d230569258f53c1aa08657f4f0ee7
Danielle Isquierdo <danielle.isquierdo@gmail.com>
2016-06-26 21:15:37 -0300

    visualizar carrito

--- ecommerce-app/public/javascripts/checkout.js
+++ ecommerce-app/public/javascripts/checkout.js
@@ -3,27 +3,69 @@
  */
 simpleCart({
 
+    cartStyle:'table',
+
     cartColumns: [
-        { attr: "name" , label: false , view: function(item, column){
-            var h3 = document.createElement("h3");
-            var a = document.createElement("a");
-            a.innerText = item.name();
-            a.surroundContents(h3);
-            return h3.innerHTML;
-        } } ,
+        /* Picture (same for every product right now) */
+        { view: function(item, column) {
+            return '<img src=\"/images/'+ item.get('image') +'\"/>';
+        }, label: false },
+        { attr: "name" , label: false } ,
         { attr: "price" , label: "Price", view: 'currency' } ,
         { view: "decrement" , label: false , text: "-" } ,
-        { attr: "quantity" , label: false, view: function(item, column){
-            var ul = document.createElement("ul");
-            ul.addClass('qty');
-            var li = document.createElement("li");
-            li.innerHTML = "<p>"+item.quantity()+"</p>";
-            li.surroundContents(ul);
-            return ul.innerHTML;
-        } } ,
+        { attr: "quantity" , label: "Qty" } ,
         { view: "increment" , label: false , text: "+" } ,
-        { attr: "total" , label: "SubTotal", view: 'currency' } ,
         { view: "remove" , text: "Remove" , label: false }
     ]
 
+
+});
+
+// $(document).ready(function(){
+//     simpleCart.find().each(function (item,x) {
+//
+//         var items = $('#items');
+//
+//         var header = createElement('div');
+//         header.addClass('cart-header');
+//
+//         var close = createElement('div');
+//         close.addClass('close1');
+//         header.append(close);
+//
+//         var i = createElement('div');
+//         i.addClass('cart-sec');
+//
+//         var imgd = createElement('div');
+//         imgd.addClass('cart-item');
+//         imgd.addClass('cyc');
+//
+//         var img = createElement('img');
+//         img.src(item.get('image'));
+//         imgd.append(img);
+//
+//         i.append(imgd);
+//
+//         var info = createElement('div');
+//         info.addClass('cart-item-info');
+//
+//         var name = createElement('h3');
+//         name.innerHTML = item.get('name');
+//         info.append(name);
+//
+//         i.append(info);
+//
+//         var clearfix = createElement('div');
+//         clearfix.addClass('clearfix');
+//
+//         i.append(clearfix);
+//         header.append(i);
+//
+//         items.append(header);
+//     });
+// });
+
+simpleCart.bind("afterCreate", function(){
+    $cart_table = $(".simpleCart_items table")
+    $cart_table.addClass("table").addClass("table-hover")
 });
