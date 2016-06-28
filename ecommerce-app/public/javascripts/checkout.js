449357f87f3424e014b8ff1184afbe0c28e977f0
Danielle Isquierdo <danielle.isquierdo@gmail.com>
2016-06-28 10:52:48 -0300

    completando vista carrito

--- ecommerce-app/public/javascripts/checkout.js
+++ ecommerce-app/public/javascripts/checkout.js
@@ -8,7 +8,7 @@ simpleCart({
     cartColumns: [
         /* Picture (same for every product right now) */
         { view: function(item, column) {
-            return '<img src=\"/images/'+ item.get('image') +'\"/>';
+            return '<img src=\"/images/'+ item.get('image') +'\"  height=\"50%\"/>';
         }, label: false },
         { attr: "name" , label: false } ,
         { attr: "price" , label: "Price", view: 'currency' } ,
@@ -21,49 +21,6 @@ simpleCart({
 
 });
 
-// $(document).ready(function(){
-//     simpleCart.find().each(function (item,x) {
-//
-//         var items = $('#items');
-//
-//         var header = createElement('div');
-//         header.addClass('cart-header');
-//
-//         var close = createElement('div');
-//         close.addClass('close1');
-//         header.append(close);
-//
-//         var i = createElement('div');
-//         i.addClass('cart-sec');
-//
-//         var imgd = createElement('div');
-//         imgd.addClass('cart-item');
-//         imgd.addClass('cyc');
-//
-//         var img = createElement('img');
-//         img.src(item.get('image'));
-//         imgd.append(img);
-//
-//         i.append(imgd);
-//
-//         var info = createElement('div');
-//         info.addClass('cart-item-info');
-//
-//         var name = createElement('h3');
-//         name.innerHTML = item.get('name');
-//         info.append(name);
-//
-//         i.append(info);
-//
-//         var clearfix = createElement('div');
-//         clearfix.addClass('clearfix');
-//
-//         i.append(clearfix);
-//         header.append(i);
-//
-//         items.append(header);
-//     });
-// });
 
 simpleCart.bind("afterCreate", function(){
     $cart_table = $(".simpleCart_items table")
